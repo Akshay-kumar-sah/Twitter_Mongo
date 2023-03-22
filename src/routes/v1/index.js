86303a27245ff1a10d1817fbe5c5496c6e19366f
Akshay-kumar-sah <akki19082001@gmail.com>
2023-03-22 11:54:37 +0530

    Added signup with encrypted password

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { createTweet,getTweet } = require('../../controllers/tweet-controller');
 const  {toggleLike} = require('../../controllers/like-controller');
 const {createComment } = require('../../controllers/comment-controller');
+const {signup} = require('../../controllers/auth-controller');
 const router = express.Router();
 
 router.post('/tweets',createTweet);
 router.post('/likes/toggle', toggleLike);
 router.post('/comments',createComment);
 router.get('/tweets/:id',getTweet);
+router.post('/signup',signup);
 
 module.exports = router;
